049bfb353fe2743825c23e69ff00eb07f996126b
Viktor Rybakov <viktor.rybakov@gmail.com>
2020-01-20 23:03:30 +0300

    1 модуль

---
+++ 1/hello.js
@@ -0,0 +1,15 @@
+var myName = prompt('Как вас зовут?');
+var mySurname = prompt('Ваша фамилия');
+var myBirthYear = prompt('Введите ваш год рождения');
+var currentYear = 2019;
+var age = currentYear - myBirthYear;
+
+if (age < 20) {
+  alert('Привет, ' + mySurname + ' ' + myName + '!');
+}
+else if (age >= 20 && age < 40) {
+  alert('Добрый день, ' + mySurname + ' ' + myName + '!');
+}
+else {
+  alert('Здравствуйте, ' + mySurname + ' ' + myName);
+}
